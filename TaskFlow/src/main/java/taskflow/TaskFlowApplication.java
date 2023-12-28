56c76c322de2725ba521e30b45ab44a7950dc6ce
spirithuntt <nouhaila.elhousnii@gmail.com>
2023-12-28 13:40:21 +0100

    removed the MapStruct because of multiple bugs

--- TaskFlow/src/main/java/taskflow/TaskFlowApplication.java
+++ TaskFlow/src/main/java/taskflow/TaskFlowApplication.java
@@ -2,14 +2,21 @@ package taskflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import taskflow.mapper.TagsMapper;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "taskflow.mapper")
+//@ComponentScan(basePackages = "taskflow.mapper")
 public class TaskFlowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskFlowApplication.class, args);
     }
+
+//    @Bean
+//    public TagsMapper tagsMapper() {
+//        return TagsMapper.INSTANCE;
+//    }
 
 }
--- TaskFlow/src/main/java/taskflow/controllers/AuthenticationController.java
+++ TaskFlow/src/main/java/taskflow/controllers/AuthenticationController.java
@@ -2,10 +2,7 @@ package taskflow.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import taskflow.dto.request.AuthenticationRequest;
 import taskflow.dto.request.RegisterRequest;
 import taskflow.dto.response.AuthenticationResponse;
@@ -28,4 +25,8 @@ public class AuthenticationController {
         return ResponseEntity.ok(authenticationService.authenticate(request));
     }
 
+    @GetMapping("/test")
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok("test");
+    }
 }
