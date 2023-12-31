4d5ef2965764ddddb1b6590abc667d69b4e47bc8
spirithuntt <nouhaila.elhousnii@gmail.com>
2023-12-31 12:14:46 +0100

    added logic that verifies the tasks deadline if its superior to today it changes the TaskStatus to Expired everyday at midnight

--- TaskFlow/src/main/java/taskflow/repository/TaskRepository.java
+++ TaskFlow/src/main/java/taskflow/repository/TaskRepository.java
@@ -1,15 +1,20 @@
 package taskflow.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import taskflow.entities.Task;
 import taskflow.entities.User;
 import taskflow.entities.enums.TaskStatus;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     List<Task> findByAssignedTo(User user);
     List<Task> findByStatus(TaskStatus status);
+
+    @Query("SELECT t FROM Task t WHERE t.deadline < :currentDate AND t.status <> 'EXPIRED'")
+    List<Task> findTasksToExpire(LocalDate currentDate);
 
 }
--- TaskFlow/src/main/java/taskflow/scheduled/ScheduledTasks.java
+++ TaskFlow/src/main/java/taskflow/scheduled/ScheduledTasks.java
@@ -3,27 +3,41 @@ package taskflow.scheduled;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import taskflow.entities.User;
+import taskflow.entities.Task;
+import taskflow.entities.enums.TaskStatus;
 import taskflow.repository.UserRepository;
+import taskflow.repository.TaskRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Component
 public class ScheduledTasks {
 
     private final UserRepository userRepository;
+    private final TaskRepository taskRepository;
 
-    public ScheduledTasks(UserRepository userRepository) {
+    public ScheduledTasks(UserRepository userRepository, TaskRepository taskRepository) {
         this.userRepository = userRepository;
+        this.taskRepository = taskRepository;
     }
 
     @Scheduled(cron = "0 0 0 * * *") // Run every midnight
-    public void updateTokens() {
+    public void updateTokensAndExpireTasks() {
+        // Update tokens for all users
         List<User> users = userRepository.findAll();
-
         for (User user : users) {
             user.setToken(2);
         }
-
         userRepository.saveAll(users);
+
+        // Expire tasks with a deadline before today
+        LocalDate currentDate = LocalDate.now();
+        List<Task> expiredTasks = taskRepository.findTasksToExpire(currentDate);
+        for (Task task : expiredTasks) {
+            task.setStatus(TaskStatus.EXPIRED);
+        }
+        taskRepository.saveAll(expiredTasks);
     }
+
 }
